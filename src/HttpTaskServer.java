88897dae46e10de7b805c98bdd831f19e84e4fc7
Catherine Edelveis <ekaterina.edelveis@gmail.com>
2024-03-17 14:44:51 +0300

    cleaned connection

--- src/HttpTaskServer.java
+++ src/HttpTaskServer.java
@@ -1,10 +1,5 @@
 
-import com.sun.net.httpserver.HttpServer;
-import httphandlers.*;
-import taskmanagement.*;
-
 import java.io.IOException;
-import java.net.InetSocketAddress;
 
 
 public class HttpTaskServer {
@@ -13,8 +8,9 @@ public class HttpTaskServer {
     public static void main(String[] args) throws IOException {
 
         //TaskManager manager = Managers.getFileBacked(new File("backup.csv"), new File("backupHistory.csv"));
-        TaskManager manager = Managers.getDefault();
+        //TaskManager manager = Managers.getDefault();
 
+        /*
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
 
         httpServer.createContext("/tasks", new TaskHandler(manager));
@@ -23,7 +19,9 @@ public class HttpTaskServer {
         httpServer.createContext("/history", new HistoryHandler(manager));
         httpServer.createContext("/prioritized", new PrioritizedHandler(manager));
 
-        //httpServer.start();
+        httpServer.start();
+
+         */
 
 
     }
